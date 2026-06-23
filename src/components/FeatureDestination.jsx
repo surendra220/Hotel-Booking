64b6ae81b9389e976eac2f8cebc5e99175ab1314
surendra220 <surendra.dev0704@gmail.com>
2026-06-23 23:24:03 +0530

    AllRooms page is added

--- src/components/FeatureDestination.jsx
+++ src/components/FeatureDestination.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets'
-import HotelCard from '..//components/HotelCard'
+import HotelCard from '../components/HotelCard'
  import Title from './Title'
  import { useNavigate } from 'react-router-dom'
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from '@clerk/clerk-react'
-
+import { ClerkProvider } from '@clerk/clerk-react';
+import "./index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
